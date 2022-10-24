54934c3cea698e52be9830867eb93f59ef5959e6
Pedro <pedbcrespo@gmail.com>
2022-10-24 18:27:06 -0300

    atualizando

---
+++ src/main/java/bootcamp/dio/controller/InicialController.java
@@ -0,0 +1,17 @@
+package bootcamp.dio.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class InicialController {
+
+    @GetMapping
+    public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
+        model.addAttribute("attribute", "veiculos");
+        return new ModelAndView("redirect:/veiculos", model);
+    }
+
+}
